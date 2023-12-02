84ff9504edaef158f319feb5016c354c8088d40f
Deepana Ishtaweera <dishtaweera@gmail.com>
2023-12-02 22:50:16 +0100

    update printing procedure

--- get-axis.py
+++ get-axis.py
@@ -7,13 +7,13 @@ point1 = [14.2,-84.084,-8.617]
 # the second point which acts as the base of the arrow
 point2 = [14.2,-107.51,-18.434]
 
-point1 = np.array(point1)
-point2 = np.array(point2)
+point1 = np.array(point1, dtype=np.float64)
+point2 = np.array(point2, dtype=np.float64)
 
 # the vector pointing from point2 to point1
 vector = point1 - point2
-print("vector: ", vector/1000)
+print("vector: ", " ".join(map(str,vector/1000)))
 
 # axis point
 axis_point = point2 + vector/2
-print("axis point: ", axis_point/1000)
+print("axis point: ", " ".join(map(str,axis_point/1000)))
