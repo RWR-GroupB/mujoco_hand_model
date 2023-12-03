95cd545c948ed9748bed8a915e936e04344b9307
Zeno <zeno.hamers@gmail.com>
2023-12-03 16:26:43 +0100

    new mujoco model (hand-v4)

--- get-axis.py
+++ get-axis.py
@@ -2,18 +2,19 @@
 import numpy as np
 
 # the point pointint towards the arrow 
-point1 = [14.2,-84.084,-8.617]
+point1 = [83.20,-101.314,-5.536]
 
 # the second point which acts as the base of the arrow
-point2 = [14.2,-107.51,-18.434]
+point2 = [83.20,-93.97,-23.06]
 
 point1 = np.array(point1, dtype=np.float64)
 point2 = np.array(point2, dtype=np.float64)
 
-# the vector pointing from point2 to point1
-vector = point1 - point2
-print("vector: ", vector/1000)
+# the vector pointing from point1 to point2
+vector = point2 - point1
+vector_u = vector/np.linalg.norm(vector)
+print("vector: ", vector_u)
 
 # axis point
-axis_point = point2 + vector/2
+axis_point = point1 + vector/2
 print("axis point: ", axis_point/1000)
